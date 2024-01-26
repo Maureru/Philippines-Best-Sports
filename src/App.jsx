19f9bb75fd147c95762268684c58c62de5eaf190
xMauu <102646969+xMauu@users.noreply.github.com>
2024-01-27 06:31:33 +0800

    tw

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,32 @@ import { links } from '../data/data';
 import { Helmet } from 'react-helmet';
 
 function App() {
-    const ids = window.location.pathname.split('/')[1];
+    /* const ids = window.location.pathname.split('/')[1]; */
+    const ids = "onexbet"
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
             // Replace 'https://example.com' with the external link you want to redirect to
-            if (!links[ids]) {
+            /* if (!links[ids]) {
                 console.log('ERROR');
             } else {
                 console.log('DEF');
                 window.location.replace(links[ids].link);
-            }
+            } */
+            window.location.replace(links[ids].link);
         }, 600);
 
         // Clear the timeout when the component is unmounted
         return () => clearTimeout(timeoutId);
     }, []);
 
-    if (!links[ids])
+/*     if (!links[ids])
         return (
             <div>
                 <h3>Invalid Link ....</h3>
             </div>
         );
-
+ */
     return (
         <div>
             <Helmet>
